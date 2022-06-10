4cb5a5b984a5b3371299945e68e64a4d6d1df2ac
David Olaniran <davidolaniran@fb.com>
2022-06-10 12:15:26 -0700

    Step 9

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -36,7 +36,7 @@ public class TwitterClient extends OAuthBaseClient {
 		String apiUrl = getApiUrl("statuses/home_timeline.json");
 		// Can specify query string params directly or through RequestParams.
 		RequestParams params = new RequestParams();
-		params.put("count", 10000);
+		params.put("count", 3000);
 		params.put("since_id", 1);
 		client.get(apiUrl, params, handler);
 	}
